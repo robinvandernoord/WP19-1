c1b60ff83b0c879bdc2d6743e51ad69f63a45f9d
Robin van der Noord <robinvandernoord@gmail.com>
2019-04-24 14:16:31 +0200

    codestyle

--- assignment_1/js/main.js
+++ assignment_1/js/main.js
@@ -16,10 +16,10 @@ function change_title() {
     // change the title of the page
     let page_name, current_page;
     /* use index as default for when you visit the folder: */
-    page_name = window.location.href.split('/').slice(-1)[0].split('.')[0] || 'index';
+    page_name = window.location.href.split("/").slice(-1)[0].split(".")[0] || "index";
     current_page = capitalize(page_name);
     document.title = make_title(current_page);
-    return current_page
+    return current_page;
 }
 
 /* assignment 2 */
@@ -27,25 +27,25 @@ function add_article() {
     // add an article to the index page
     let new_article, new_heading, new_paragraph;
 
-    new_article = document.createElement('article');
+    new_article = document.createElement("article");
 
-    new_heading = document.createElement('h1');
-    new_heading.innerText = 'Hello there';
+    new_heading = document.createElement("h1");
+    new_heading.innerText = "Hello there";
     new_article.appendChild(new_heading);
 
-    new_paragraph = document.createElement('p');
-    new_paragraph.innerText = 'De planning van de komende weken is als volgt:';
+    new_paragraph = document.createElement("p");
+    new_paragraph.innerText = "De planning van de komende weken is als volgt:";
     new_article.appendChild(new_paragraph);
 
-    document.getElementsByClassName('container')[0].appendChild(new_article);
+    document.getElementsByClassName("container")[0].appendChild(new_article);
     return new_article;
 }
 
 /* assignment 3 + 4 */
 function change_footer() {
     // add a link to the footer and open it in a new tab
-    let third_link = document.querySelectorAll('footer #myTabContent li a')[2];
-    third_link.href = 'https://google.com';
+    let third_link = document.querySelectorAll("footer #myTabContent li a")[2];
+    third_link.href = "https://google.com";
     third_link.target = "_blank";
 }
 
@@ -53,8 +53,8 @@ function change_footer() {
 function make_red() {
     // make the children of nav-item red
     /* nav-item doesn't make the text red, nav-link does */
-    for (let element of document.getElementsByClassName('nav-link')) {
-        element.style.color = 'red';
+    for (let element of document.getElementsByClassName("nav-link")) {
+        element.style.color = "red";
     }
 }
 
@@ -62,20 +62,20 @@ function make_red() {
 function make_object() {
     // create an object
     return {
-        'Week 1': 'Assignment 1',
-        'Week 2': 'No lecture',
-        'Week 3': 'Assignment 2',
-        'Week 4': '-',
-        'Week 5': 'Assignment 3',
-        'Week 6': '-',
-        'Week 7': 'Guest Lecture'
+        "Week 1": "Assignment 1",
+        "Week 2": "No lecture",
+        "Week 3": "Assignment 2",
+        "Week 4": "-",
+        "Week 5": "Assignment 3",
+        "Week 6": "-",
+        "Week 7": "Guest Lecture"
     };
 }
 
 function display_object(obj, element) {
     // show an object
-    let newparagraph = document.createElement('p');
-    let representation = '';
+    let newparagraph = document.createElement("p");
+    let representation = "";
     for (let key in obj) {
         if (obj.hasOwnProperty(key)) {
             // make sure obj has this key with a value
@@ -97,26 +97,26 @@ function wrap(el, wrapper) {
 function add_sidebar() {
     // create a sidebar and wrap a row around the content and sidebar
     let container, new_sidebar, new_header, new_row, content, sidebar_article;
-    container = document.querySelector('.container');
-    content = container.querySelector('.col-md-12');
+    container = document.querySelector(".container");
+    content = container.querySelector(".col-md-12");
     /* change col-12 to col-8 */
-    content.classList.replace('col-md-12', 'col-md-8');
+    content.classList.replace("col-md-12", "col-md-8");
 
     /* new div element with col-md-4 */
-    new_sidebar = document.createElement('div');
-    new_sidebar.classList.add('col-md-4');
+    new_sidebar = document.createElement("div");
+    new_sidebar.classList.add("col-md-4");
     /* add <article> for margin etc */
-    sidebar_article = document.createElement('article');
+    sidebar_article = document.createElement("article");
     new_sidebar.append(sidebar_article);
 
     /* new heading */
-    new_header = document.createElement('h1');
-    new_header.innerText = 'Sidebar';
+    new_header = document.createElement("h1");
+    new_header.innerText = "Sidebar";
     sidebar_article.appendChild(new_header);
 
     /* new row so the sidebar is actually next to the content */
-    new_row = document.createElement('div');
-    new_row.classList.add('row');
+    new_row = document.createElement("div");
+    new_row.classList.add("row");
     wrap(content, new_row);
     new_row.appendChild(new_sidebar);
 }
@@ -129,13 +129,13 @@ function main() {
     change_footer();
     make_red();
     myobject = make_object();
-    if (current_page === 'Index') {
+    if (current_page === "Index") {
         article_element = add_article();
         display_object(myobject, article_element);
-    } else if (current_page === 'Second') {
+    } else if (current_page === "Second") {
         add_sidebar();
     }
 }
 
 // start it all after the page has finished loading:
-window.addEventListener('load', main);
+window.addEventListener("load", main);
