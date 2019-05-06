34bf0cb6794c7eceb581445f5e2b9fac355f7e43
Robin van der Noord <robinvandernoord@gmail.com>
2019-05-06 19:51:39 +0200

    added latest non-slim version of jquery

--- assignment_2/tpl/head.php
+++ assignment_2/tpl/head.php
@@ -12,9 +12,7 @@
 	<link rel="stylesheet" href="css/styles.css">
 
 	<!-- Scripts -->
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-	        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-	        crossorigin="anonymous"></script>
+	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
 	<script type="application/javascript" src="js/main.js"></script>
 	<?php if($navigation['active'] == 'Links'):?>
 	<script type="application/javascript" src="js/links.js"></script>
