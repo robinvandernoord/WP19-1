7b70b850e9a3db8bf09620a05f6326cf5ea6b694
Robin van der Noord <robinvandernoord@gmail.com>
2019-05-15 11:21:51 +0200

    added template files (pt 1 and 2.1)

---
+++ assignment_3/simple_form.php
@@ -0,0 +1,19 @@
+<?php
+/* Header */
+$page_title = 'Webprogramming Assignment 3';
+$navigation = Array(
+    'active' => 'Simple Form',
+    'items' => Array(
+        'News' => '/WP19/assignment_3/index.php',
+        'Add news item' => '/WP19/assignment_3/news_add.php',
+        'Leap Year' => '/WP19/assignment_3/leapyear.php',
+        'Simple Form' => '/WP19/assignment_3/simple_form.php'
+    )
+);
+include __DIR__ . '/tpl/head.php';
+include __DIR__ . '/tpl/body_start.php';
+?>
+
+<?php
+include __DIR__ . '/tpl/body_end.php';
+?>
